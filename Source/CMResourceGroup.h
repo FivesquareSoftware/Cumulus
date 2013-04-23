fac1915ac3b9d0d7fb9e5c61d5954f90d127fa77
John Clayton <john@fivesquare.net>
2013-04-22 22:22:15 -0700

    Completes #6, resource group now simply takes a block to perform

--- Source/CMResourceGroup.h
+++ Source/CMResourceGroup.h
@@ -13,19 +13,9 @@
 @interface CMResourceGroup : NSObject
 
 @property (nonatomic, strong) NSString *name;
-@property (nonatomic, readonly) NSSet *resources;
-@property (nonatomic, readonly) NSSet *completionBlocks;
-@property (nonatomic) BOOL clearsBlocksOnCompletion; ///< When YES, after a mark is hit, all current completion blocks are removed after being invoked
-
-+ (id) withCompletionBlock:(void(^)())completionBlock;
-
-- (void) addResource:(CMResource *)resource; ///< The resource's requests will now be tracked by the receiver
-- (void) addCompletionBlock:(void(^)())completionBlock;
 
 
-/** Begins waiting for completion of currently scheduled requests. Safe to call repeatedly, but the blocks will be invoked once for every mar unless #clearsBlocksOnCompletion = YES. */
-- (void) mark;
-- (void) markAfterDelay:(NSTimeInterval)delay; ///< calls #mark after a delay, useful if you are firing a lot of requests rapidly and need to give them a chance to launch before marking
++ (id) withName:(NSString *)name;
 
 - (void) performWork:(void(^)(CMResourceGroup *group))groupWork withCompletionBlock:(void(^)(BOOL success, NSArray *responses))completionBlock;
 
--- Source/CMResourceGroup.m
+++ Source/CMResourceGroup.m
@@ -16,8 +16,6 @@
 @interface CMResourceGroup ()
 @property (nonatomic, assign) dispatch_queue_t dispatchQueue;
 @property (nonatomic, assign) dispatch_group_t dispatchGroup;
-@property (nonatomic, strong) NSMutableSet *resourcesInternal;
-@property (nonatomic, strong) NSMutableSet *completionBlocksInternal;
 @property (nonatomic, strong) NSMutableArray *currentResponses;
 @end
 
@@ -28,22 +26,8 @@
 #pragma mark - Properties
 
 @synthesize name = _name;
-@synthesize clearsBlocksOnCompletion = _clearsBlocksOnCompletion;
-
-@dynamic resources;
-- (NSSet *) resources {
-	return [_resourcesInternal copy];
-}
-
-@dynamic completionBlocks;
-- (NSSet *) completionBlocks {
-	return [_completionBlocksInternal copy];
-}
-
 @synthesize dispatchQueue = _dispatchQueue;
 @synthesize dispatchGroup = _dispatchGroup;
-@synthesize completionBlocksInternal = _completionBlocksInternal;
-@synthesize resourcesInternal = _resourcesInternal;
 
 
 // ========================================================================== //
@@ -51,35 +35,31 @@
 #pragma mark - Object
 
 - (void)dealloc {
+	dispatch_set_context(_dispatchQueue, NULL);
 	dispatch_release(_dispatchQueue);
 	dispatch_group_wait(_dispatchGroup, DISPATCH_TIME_FOREVER);
     dispatch_release(_dispatchGroup);
 }
 
-+ (id) withCompletionBlock:(void(^)())completionBlock {
-	CMResourceGroup *group = [self new];
-	if (completionBlock) {
-		[group addCompletionBlock:completionBlock];
-	}
-	return group;
++ (id) withName:(NSString *)name {
+	return [[self alloc] initWithName:name];
 }
 
-- (id)init {
+- (id)initWithName:(NSString *)name {
     self = [super init];
     if (self) {
+		_name = name;
 		_dispatchQueue = dispatch_queue_create("com.fivesquaresoftware.Cumulus.CMResourceGroup", DISPATCH_QUEUE_SERIAL);
+		dispatch_set_context(_dispatchQueue, (__bridge void *)(self));
 		_dispatchGroup = dispatch_group_create();
-		_completionBlocksInternal = [NSMutableSet new];
-		_resourcesInternal = [NSMutableSet new];
-		_clearsBlocksOnCompletion = YES;
 		_currentResponses = [NSMutableArray new];
     }
     return self;
 }
 
-//- (NSString *) description {
-//	return [NSString stringWithFormat:@"%@ : { name : %@, resourceCount: %u, blockCount: %u }",[super description], _name,[_resourcesInternal count], [_completionBlocksInternal count]];
-//}
+- (NSString *) description {
+	return [NSString stringWithFormat:@"%@ name : %@",[super description], _name];
+}
 
 
 // ========================================================================== //
@@ -87,72 +67,16 @@
 #pragma mark - Public
 
 
-
-- (void) addResource:(CMResource *)resource {
-	resource.resourceGroup = self;
-	[_resourcesInternal addObject:resource];
-}
-
-- (void) addCompletionBlock:(void(^)())completionBlock {
-	[_completionBlocksInternal addObject:[completionBlock copy]];
-}
-
-- (void) enter {
-	RCLog(@"enter ->");
-	dispatch_group_enter(_dispatchGroup);
-}
-
-- (void) leave {
-	RCLog(@"leave ->");
-	dispatch_group_leave(_dispatchGroup);
-}
-
-- (void) mark {
-	dispatch_async(_dispatchQueue, ^{
-		dispatch_group_wait(_dispatchGroup, DISPATCH_TIME_FOREVER);
-		RCLog(@"** run completion blocks **");
-		NSSet *blocks = [_completionBlocksInternal copy];
-		if (_clearsBlocksOnCompletion) {
-			[_completionBlocksInternal removeAllObjects];
-		}
-		for (id block in blocks) {
-			dispatch_async(dispatch_get_main_queue(),block);
-		}
-	});
-}
-
-- (void) markAfterDelay:(NSTimeInterval)delay {
-	double delayInSeconds = delay;
-	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-		[self mark];
-	});
-}
-
 - (void) performWork:(void(^)(CMResourceGroup *group))groupWork withCompletionBlock:(void(^)(BOOL success, NSArray *responses))completionBlock {
-	// set group somewhere so that any resource inside work block can see it and attach
-	// run work block
-	// run completion block
-	
-	dispatch_queue_t requestQueue = [CMResource dispatchQueue];
 	dispatch_async(_dispatchQueue, ^{
-		
-		// Install a choke point on request dispatch Q to set the resource group
-		// This allows any blocks on the queue already to complete, subsequent request dispatches will see us as their group
-		dispatch_barrier_sync(requestQueue, ^{
-			dispatch_set_context(requestQueue, (__bridge void *)(self));
-		});
-		
 		// Clear the responses and dispatch the current work block
 		[_currentResponses removeAllObjects];
+
+		// dispatch the work, resources check their current q for a group before dispatching requests
 		groupWork(self);
 		
 		// Wait for the group to complete
 		dispatch_group_wait(_dispatchGroup, DISPATCH_TIME_FOREVER);
-		// Zero out the request dispatch Q's group
-		dispatch_barrier_sync(requestQueue, ^{
-			dispatch_set_context(requestQueue, NULL);
-		});
 		
 		// Collect our success rate
 		__block BOOL success = YES;
@@ -171,9 +95,29 @@
 	
 }
 
+// ========================================================================== //
+
+#pragma mark - Protected
+
+
+- (void) enter {
+	RCLog(@"enter ->");
+	dispatch_group_enter(_dispatchGroup);
+}
+
+- (void) leave {
+	RCLog(@"leave ->");
+	dispatch_group_leave(_dispatchGroup);
+}
+
 - (void) leaveWithResponse:(CMResponse *)response {
+	RCLog(@"leaveWithResponse: ->");
 	[_currentResponses addObject:response];
 	dispatch_group_leave(_dispatchGroup);
 }
+
+
+
+
 
 @end
