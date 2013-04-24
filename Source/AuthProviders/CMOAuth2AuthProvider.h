4f65357ce550983ca3834fef4bdb80d74c532b48
John Clayton <john@fivesquare.net>
2013-04-24 14:44:09 -0700

    More messing about with headers trying to get podspec to validate, 3

--- Source/AuthProviders/CMOAuth2AuthProvider.h
+++ Source/AuthProviders/CMOAuth2AuthProvider.h
@@ -34,9 +34,9 @@
  */
 
 #import <Foundation/Foundation.h>
-#if TARGET_OS_IPHONE == 0
-#import <WebKit/WebKit.h>
-#endif
+//#if TARGET_OS_IPHONE == 0
+//#import <WebKit/WebKit.h>
+//#endif
 
 
 #import "CMAuthProvider.h"
