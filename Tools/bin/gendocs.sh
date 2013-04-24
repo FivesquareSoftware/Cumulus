1d440ff2b8fa075d2270379dc6fe94694c13a825
John Clayton <john@fivesquare.net>
2013-04-24 11:47:53 -0700

    Ignoring doc install semaphore

--- Tools/bin/gendocs.sh
+++ Tools/bin/gendocs.sh
@@ -8,11 +8,11 @@
 
 
 appledoc="${SRCROOT}/Tools/bin/appledoc"
-docpath="${SRCROOT}/Docs/API"
+docpath="${SRCROOT}/Docs/API/"
 srcpath="${SRCROOT}/Source"
 
-echo "Documenting Cumulus version ${VERSION}"
+echo "Documenting Cumulus version ${VERSION} to $docpath"
 
 
-$appledoc --project-name=Cumulus --project-version=$VERSION --project-company='Fivesquare Software' --company-id='com.fivesquaresoftware' --docset-install-path $docpath -o $docpath --no-repeat-first-par --index-desc "${SRCROOT}/Docs/API/index.md" --ignore '.m' --ignore '*+Private.h' --ignore '*+Protected.h' --keep-undocumented-objects --keep-undocumented-members --print-information-block-titles  --crossref-format "#?%@" --exit-threshold 2 "${SRCROOT}/Source"
+$appledoc --project-name=Cumulus --project-version=$VERSION --project-company='Fivesquare Software' --company-id='com.fivesquaresoftware' --no-repeat-first-par --index-desc "${SRCROOT}/Docs/API/index.md" --ignore '.m' --ignore '*+Private.h' --ignore '*+Protected.h' --keep-undocumented-objects --keep-undocumented-members --print-information-block-titles  --crossref-format "#?%@" --exit-threshold 2 --docset-install-path $docpath -o $docpath $srcpath
 
