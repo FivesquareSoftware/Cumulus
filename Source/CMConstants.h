1a68055b952312652ead7d79297f3320d4df8578
John Clayton <john@fivesquare.net>
2013-05-06 13:49:04 -0700

    Added a macro to test for ARC behavior wrt to dispatch objects

--- Source/CMConstants.h
+++ Source/CMConstants.h
@@ -46,6 +46,27 @@
 	#define RCLog(fmt,...) 
 #endif
 
+#if TARGET_OS_IPHONE
+	#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000
+		#define ARC_SUPPORTS_DISPATCH_OBJECTS 1
+	#else
+		#define ARC_SUPPORTS_DISPATCH_OBJECTS 0
+	#endif
+#else
+	#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+		#define ARC_SUPPORTS_DISPATCH_OBJECTS 1
+	#else
+		#define ARC_SUPPORTS_DISPATCH_OBJECTS 0
+	#endif
+#endif
+
+
+#if ARC_SUPPORTS_DISPATCH_OBJECTS
+	#ifdef dispatch_release
+		#undef dispatch_release
+	#endif
+	#define dispatch_release(object)
+#endif
 
 extern NSString *kCumulusErrorDomain;
 extern NSString *kRESTCLientHTTPStatusCodeErrorKey;
