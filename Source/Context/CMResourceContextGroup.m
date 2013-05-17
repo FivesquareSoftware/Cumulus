b19bb3a364a5e3e8a8d175db2cfba560f120931e
John Clayton <john@fivesquare.net>
2013-05-17 13:21:38 -0700

    WIP #14 adding a semaphore around request/response objects in group

--- Source/Context/CMResourceContextGroup.m
+++ Source/Context/CMResourceContextGroup.m
@@ -12,6 +12,7 @@
 
 @interface CMResourceContextGroup () {
 	dispatch_group_t _dispatchGroup;
+	dispatch_semaphore_t _dispatchSemaphore;
 }
 @property (nonatomic, strong) NSMutableSet *requestsInternal;
 @property (nonatomic, strong) NSMutableSet *responsesInternal;
@@ -21,19 +22,24 @@
 
 @dynamic runningRequests;
 - (NSSet *) runningRequests {
+	dispatch_semaphore_wait(_dispatchSemaphore, DISPATCH_TIME_FOREVER);
 	NSSet *runningRequests = [_requestsInternal copy];
+	dispatch_semaphore_signal(_dispatchSemaphore);
 	return runningRequests;
 }
 
 @dynamic responses;
 - (NSSet *) responses {
+	dispatch_semaphore_wait(_dispatchSemaphore, DISPATCH_TIME_FOREVER);
 	NSSet *responses = [_responsesInternal copy];
+	dispatch_semaphore_signal(_dispatchSemaphore);
 	return responses;
 }
 
 - (void)dealloc {
 	dispatch_group_wait(_dispatchGroup, DISPATCH_TIME_FOREVER);
     dispatch_release(_dispatchGroup);
+	dispatch_release(_dispatchSemaphore);
 }
 
 - (id)init {
@@ -41,6 +47,9 @@
     if (self) {
 		_identifier = [NSUUID new];
 		_dispatchGroup = dispatch_group_create();
+//		NSString *queueName = [NSString stringWithFormat:@"com.fivesquaresoftware.Cumulus.CMResourceContextGroup.%@",_identifier];
+//		_dispatchQueue = dispatch_queue_create([queueName UTF8String], DISPATCH_QUEUE_CONCURRENT);
+		_dispatchSemaphore = dispatch_semaphore_create(1);
 		_requestsInternal = [NSMutableSet new];
 		_responsesInternal = [NSMutableSet new];
     }
@@ -54,7 +63,9 @@
 - (void) enterWithRequest:(CMRequest *)request {
 	RCLog(@"%@.enterWithRequest: ->",self);
 	if (request) {
+		dispatch_semaphore_wait(_dispatchSemaphore, DISPATCH_TIME_FOREVER);
 		[_requestsInternal addObject:request];
+		dispatch_semaphore_signal(_dispatchSemaphore);
 	}
 	if (_wasCanceled) {
 		[request cancel];
@@ -65,8 +76,10 @@
 - (void) leaveWithResponse:(CMResponse *)response {
 	RCLog(@"%@.leaveWithResponse: <-",self);
 	if (response) {
+		dispatch_semaphore_wait(_dispatchSemaphore, DISPATCH_TIME_FOREVER);
 		[_responsesInternal addObject:response];
 		[_requestsInternal removeObject:response.request];
+		dispatch_semaphore_signal(_dispatchSemaphore);
 	}
 	dispatch_group_leave(_dispatchGroup);
 }
@@ -77,9 +90,11 @@
 
 - (void) cancel {
 	_wasCanceled = YES;
+	dispatch_semaphore_wait(_dispatchSemaphore, DISPATCH_TIME_FOREVER);
 	[_requestsInternal enumerateObjectsUsingBlock:^(CMRequest *request, BOOL *stop) {
 		[request cancel];
 	}];
+	dispatch_semaphore_signal(_dispatchSemaphore);
 }
 
 @end
